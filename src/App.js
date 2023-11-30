8bbe1dcd494eca335744a6bc91f6bfd92efacb57
avii001 <anish.choudhury001@gamil.com>
2023-11-30 17:12:03 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <title>mywebsite</title>
+      </header>
+      <body>
+        <h1>WELCOME TO MY WEB SITE</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
